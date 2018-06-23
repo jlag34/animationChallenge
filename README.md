2cb586c4e6e2334d2a8aab63569997b211721076
Joe Lagasse <joe@servant.co>
2018-06-23 17:13:24 -0400

    Finished exercise

---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started
+
+Run `npm install` to get all of the dependencies. 
+Run `npm run dev` to get the project up and running.
+Lastly, navigate to http://localhost:8080 in your browser.
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import './App.scss';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { active: 'Audiences' };
+
+		this.handleActive = this.handleActive.bind(this);
+	}
+
+	handleActive(active) {
+		this.setState({ active });
+	}
+
 	render() {
+		const { active } = this.state;
+		const navTitles = ['Audiences', 'Partners', 'Brands'];
+
 		return (
-			<div>Hello world</div>
+			<div>
+				{/* CSS Transition is only used for the mounting of the component */}
+				<CSSTransition
+					in
+					classNames="fade"
+					appear={true}
+					timeout={500}
+				>
+					<div className="navbar-wrapper">
+						{navTitles.map(title => {
+							// CSS transitions used for state changes
+							const isHighlighted = active === title ? 'active' : 'inactive';
+							return (
+								<div
+									className={isHighlighted}
+									key={title}
+									onClick={() => this.handleActive(title)}
+								>
+									{title}
+								</div>
+							);
+						})}
+					</div>
+				</CSSTransition>
+			</div>
 		);
 	}
 }
